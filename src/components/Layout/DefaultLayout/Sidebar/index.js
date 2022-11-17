8e156cfc6243bfe5ed5b5b05c286fa569b492bd7
Hoai Vi <hoaivi.it@gmail.com>
2022-11-17 19:40:08 +0700

    Build base layout

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,10 @@
+import styles from './Sidebar.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-    return <h1>Sidebar</h1>;
+    return <div className={cx('wrapper')}> Sidebar</div>;
 }
 
 export default Sidebar;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,13 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 function Header() {
-    return <h1>Header</h1>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('content')}>Header</div>
+        </header>
+    );
 }
 
 export default Header;
